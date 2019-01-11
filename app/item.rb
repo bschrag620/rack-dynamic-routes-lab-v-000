c0839b26e2c756f3757b250b1fa6da8af9be0278
Brad Schrag <brad.schrag@gmail.com>
2019-01-11 22:21:56 +0000

    Automatically backed up by Learn

--- app/item.rb
+++ app/item.rb
@@ -1,8 +1,26 @@
 class Item
   attr_accessor :name, :price
+  
+  @@item = []
 
   def initialize(name,price)
     @name = name
     @price = price
+    @@item << self
+  end
+  
+  def self.all
+    @@item
+  end
+  
+  def self.find_by_name(name)
+    
+  
+  def self.return_price_by_name(name)
+    @@item.each do |item|
+      if item.name == name
+        return item
+      end
+    end
   end
 end
